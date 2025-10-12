5fb062e7280cc455ee5dbd9573c4ff2d990ea805
Idost12 <idosteinfeld2001@gmail.com>
2025-10-12 22:10:17 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./GCSDApp";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import GCSDApp from './GCSDApp';
 
-const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <GCSDApp />
+  </React.StrictMode>
+);
