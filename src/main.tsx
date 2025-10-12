54bf9c96b299aac4516ca0ded2fe44f56ee5be44
Idost12 <idosteinfeld2001@gmail.com>
2025-10-12 22:07:31 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import GCSDApp from './GCSDApp'
-import { Toaster } from 'sonner'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./GCSDApp";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Toaster position="top-right" richColors />
-    <GCSDApp />
-  </React.StrictMode>,
-)
+const root = createRoot(document.getElementById("root")!);
+root.render(<App />);
