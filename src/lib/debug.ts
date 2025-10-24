4de301c1cad98d8e8ffbc081547ead56493f8469
Idost12 <idosteinfeld2001@gmail.com>
2025-10-24 15:35:19 +0300

    Create debug.ts

---
+++ src/lib/debug.ts
@@ -0,0 +1,9 @@
+// src/lib/debug.ts
+// Simple debug file to test if the build is working
+
+console.log("🔧 Debug file loaded successfully!");
+
+export function testDebug() {
+  console.log("🔧 Debug function called!");
+  return "Debug working!";
+}
